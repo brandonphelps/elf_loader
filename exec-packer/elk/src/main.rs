e30f22003fa182b5e3c4fed2e00a82961b8b241b
Brandon Phelps <bwp44f@mst.edu>
2022-12-28 22:33:48 -0600

    updated for extracting program headers rather than hard coded addresses.

--- exec-packer/elk/src/main.rs
+++ exec-packer/elk/src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, error::Error, fs};
 
-fn ndisasm(code: &[u8]) -> Result<(), Box<dyn Error>> {
+fn ndisasm(code: &[u8], origin: delf::Addr) -> Result<(), Box<dyn Error>> {
     use std::{
         io::Write,
         process::{Command, Stdio}
@@ -9,6 +9,8 @@ fn ndisasm(code: &[u8]) -> Result<(), Box<dyn Error>> {
     let mut child = Command::new("ndisasm")
         .arg("-b")
         .arg("64")
+        .arg("-o")
+        .arg(format!("{}", origin.0))
         .arg("-")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
@@ -41,22 +43,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     use std::process::Command;
 
     println!("Disassembling {:?}...", input_path);
-    let code = &input[0x1000..];
-
-    let code = &code[..std::cmp::min(0x25, code.len())];
-    ndisasm(code)?;
+    let code_ph = file.program_headers.iter().find(|ph| ph.mem_range().contains(&file.entry_point)).expect("Segement with entry point not found");
+    ndisasm(&code_ph.data[..], file.entry_point)?;
 
     let status = Command::new(&input_path).status()?;
     if !status.success() {
         return Err("process did not exit successfully".into());
     }
 
+    /*
     println!("Executing {:?} in memory...", input_path);
     let entry_point = code.as_ptr();
     println!("Entry point: {:?}", entry_point);
     unsafe  {
         jmp(entry_point);
     }
+     */
 
 
     Ok(())
