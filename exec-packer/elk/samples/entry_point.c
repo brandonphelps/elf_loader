553279eefaaddd11f0b71d296a5d8b1c5b452be1
Brandon Phelps <bwp44f@mst.edu>
2022-12-28 21:44:41 -0600

    fixed bug in entry point program that caused segfault.

--- exec-packer/elk/samples/entry_point.c
+++ exec-packer/elk/samples/entry_point.c
@@ -1,14 +1,36 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+#include <sys/mman.h>
+#include <errno.h>
 
 const int cons = 29;
 
-const char* instructions = "\x48\x31\xFF\xB8\x3C\x00\x00\x0F\x05";
+const char* instructions = "\x48\x31\xFF\xB8\x3C\x00\x00\x00\x0F\x05";
+const size_t instructions_len = 10;
 
 int main() {
   printf("        main @ %p\n", &main);
   printf("instructions @ %p\n", instructions);
+
+  size_t region = (size_t)instructions;
+  region = region & (~0xFFF);
+  printf("        page @ %p\n", region);
+
+  printf("makeing instructions executable...\n");
+  int ret = mprotect(
+		     (void*)region, // addr
+		     0x1000,
+		     PROT_READ | PROT_EXEC
+		     );
+
+  if (ret != 0) {
+    printf("mprotect failed: error %d\n", errno);
+    return 1;
+  }
+
   void (*f)(void) = (void*)instructions;
   printf("jumping...\n");
 
